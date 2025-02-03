9f0dc7d883ed8ce68fad6f03536ee4bf6c4b8624
Leximir <aleksa.stankovic@proton.me>
2025-02-03 20:14:52 +0100

    Podesen ForecastsModel.php da popunjava nove tabele

--- app/Models/ForecastsModel.php
+++ app/Models/ForecastsModel.php
@@ -9,7 +9,11 @@ class ForecastsModel extends Model
     protected $table = 'forecasts';
 
     protected $fillable = [
-        'city_id', 'temperature', 'date'
+        'city_id', 'temperature', 'date', 'weather_type', 'probability'
+    ];
+
+    const WEATHERS = [
+        'rainy' , 'sunny', 'snowy'
     ];
 
     public function city()
--- database/seeders/ForecastsSeeder.php
+++ database/seeders/ForecastsSeeder.php
@@ -26,10 +26,22 @@ class ForecastsSeeder extends Seeder
             $this->command->getOutput()->info($name);
             $this->command->getOutput()->progressStart(5);
             for($i = 0; $i < 5; $i++){
+
+                $weatherType = ForecastsModel::WEATHERS[rand(0, 2)];
+
+                $probability = null;
+
+                if($weatherType == 'rainy' || $weatherType == 'snowy')
+                {
+                    $probability = rand(1, 100);
+                }
+
                 ForecastsModel::create([
                     'city_id' => $id ,
                     'temperature' => $faker->randomFloat(1,15,30),
-                    'date' => Carbon::now()->addDays(rand(1,30)) // Carbon je dobar za datume
+                    'date' => Carbon::now()->addDays(rand(1,30)), // Carbon je dobar za datume
+                    'weather_type' => $weatherType ,
+                    'probability' => $probability
                 ]);
                 $this->command->getOutput()->progressAdvance();
             }

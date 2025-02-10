c4809ec51156f17b63268619b2a70c72d8add77f
Leximir <aleksa.stankovic@proton.me>
2025-02-10 22:12:24 +0100

    Zavrsen Domaci #15 sa bonusom i blagim optimizacijama sa switch-om

--- database/seeders/ForecastsSeeder.php
+++ database/seeders/ForecastsSeeder.php
@@ -17,13 +17,18 @@ class ForecastsSeeder extends Seeder
     {
         $cities = CitiesModel::all();
 
+        $forecastsNumber = $this->command->getOutput()->ask("Koliko prognoza zelite da dodate svakom gradu u tabeli ?" , 5);
+
         foreach ($cities as $city){
+
+            $lastTemperature = null;
+
             $id = $city->id;
             $name = $city->name;
 
             $this->command->getOutput()->info($name);
             $this->command->getOutput()->progressStart(5);
-            for($i = 0; $i < 5; $i++){
+            for($i = 0; $i < $forecastsNumber; $i++){
 
                 $weatherType = ForecastsModel::WEATHERS[rand(0, 3)];
 
@@ -34,14 +39,30 @@ class ForecastsSeeder extends Seeder
                     $probability = rand(1, 100);
                 }
 
-                $temperature = rand(-300, 400) / 10;
-                if($weatherType === 'cloudy'){
-                    $temperature = rand(-100, 150) / 10;
-                } else if($weatherType === 'snowy'){
-                    $temperature = rand(-300, 0) / 10;
-                } else if($weatherType === 'rainy'){
-                    $temperature = rand(0, 10) / 10;
+                $temperature = null;
+
+                if($lastTemperature !== null){
+                    $minTemperature = $lastTemperature - 5;
+                    $maxTemperature = $lastTemperature + 5;
+                    $temperature = rand($minTemperature, $maxTemperature);
+                } else {
+                    switch ($weatherType){
+                        case 'sunny':
+                            $temperature = rand(-500, 500) / 10;
+                            break;
+                        case 'cloudy':
+                            $temperature = rand(-500, 150) / 10;
+                            break;
+                        case 'rainy':
+                            $temperature = rand(-100, 500) / 10;
+                            break;
+                        case 'snowy':
+                            $temperature = rand(-500, 10) / 10;
+                            break;
+                    }
                 }
+
+
 
                 ForecastsModel::create([
                     'city_id' => $id ,
@@ -51,6 +72,9 @@ class ForecastsSeeder extends Seeder
                     'probability' => $probability
                 ]);
                 $this->command->getOutput()->progressAdvance();
+
+                $lastTemperature = $temperature;
+
             }
             $this->command->getOutput()->progressFinish();
         }

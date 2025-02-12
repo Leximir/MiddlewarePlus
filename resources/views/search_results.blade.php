4cbbc51933594ac53173de34ab4ca1b45793a935
Leximir <aleksa.stankovic@proton.me>
2025-02-12 20:45:20 +0100

    Dodano dugme pored gradova , pocetna faza za lajkovanje

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -2,15 +2,20 @@
 
 @section('content')
 
-    @foreach($cities as $city)
+    <div class="d-flex flex-wrap container mt-5">
+        @foreach($cities as $city)
 
-        @php
-        $icon = \App\Http\Helpers\ForecastsHelper::getIconByWeatherType($city->todaysForecast->weather_type);
-        @endphp
+            @php
+            $icon = \App\Http\Helpers\ForecastsHelper::getIconByWeatherType($city->todaysForecast->weather_type);
+            @endphp
 
-        <p><a href="{{ route('forecast.permalink', ['city' => $city->name]) }}">
-                <i class="fa-solid {{$icon}}"></i> {{ $city->name }}
-        </a></p>
-    @endforeach
+            <p>
+                <a href=""><i class="fa-regular fa-heart btn btn-primary text-white"></i></a>
+                <a class="btn btn-primary text-white me-4" href="{{ route('forecast.permalink', ['city' => $city->name]) }}">
+                    <i class="fa-solid {{$icon}}"></i> {{ $city->name }}
+                </a>
+            </p>
+        @endforeach
+    </div>
 
 @endsection
